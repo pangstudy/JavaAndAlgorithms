8c88ccb84fea7aedceb7f653a5324dc0a85ce243
yeongmin <dudals1069@gmail.com>
2021-02-17 19:17:24 +0900

    2-17

---
+++ fibo2.java
@@ -0,0 +1,22 @@
+package practice6;
+
+public class fibo2 {
+	
+	
+	public static int fibonacci(int n) {
+		if(n==1) {
+			return 1;
+		}
+		else if(n==2){
+			return 1;
+		}
+		else {
+			return fibonacci(n-1) + fibonacci(n-2);
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(fibonacci(10));
+	}
+
+}
